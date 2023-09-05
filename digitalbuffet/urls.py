999679256e7d438b7347a658649c74ea0fe24f39
NaiPofo <50967056+naipofo@users.noreply.github.com>
2023-09-05 10:33:25 +0000

    basic view templates

--- digitalbuffet/urls.py
+++ digitalbuffet/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("buffet", include("frontbuffet.urls"))
+    path("buffet/", include("frontbuffet.urls"))
 ]
--- frontbuffet/views.py
+++ frontbuffet/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import FoodOffer
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the buffet index.")
+    latest_question_list = FoodOffer.objects.all()
+    template = loader.get_template("frontbuffet/index.html")
+    context = {
+        "all_offers_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, offer_id):
+    try:
+        offer = FoodOffer.objects.get(pk=offer_id)
+    except FoodOffer.DoesNotExist:
+        raise Http404("Offer does not exist")
+    return render(request, "frontbuffet/detail.html", {"offer": offer})
