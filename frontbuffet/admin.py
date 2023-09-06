3f833acf5b1d3bdfa1b6cd3bfbca2defda6a970c
NaiPofo <50967056+naipofo@users.noreply.github.com>
2023-09-06 07:20:39 +0000

    Formatting and basic administration for orders

--- frontbuffet/admin.py
+++ frontbuffet/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import FoodOffer
+from .models import FoodOffer, OrderEntry, PlacedOrder
 
 admin.site.register(FoodOffer)
+admin.site.register(OrderEntry)
+admin.site.register(PlacedOrder)
--- frontbuffet/models.py
+++ frontbuffet/models.py
@@ -10,8 +10,14 @@ class FoodOffer(models.Model):
 class PlacedOrder(models.Model):
     order_code = models.CharField(max_length=4)
 
+    def __str__(self):
+        return f"Order: " + ", ".join(str(x) for x in self.orderentry_set.all())
+
 
 class OrderEntry(models.Model):
     order = models.ForeignKey(PlacedOrder, on_delete=models.CASCADE)
     product = models.ForeignKey(FoodOffer, on_delete=models.CASCADE)
     amount = models.IntegerField()
+
+    def __str__(self):
+        return self.product.title
